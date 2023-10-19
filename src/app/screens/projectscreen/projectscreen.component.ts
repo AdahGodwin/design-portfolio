aa2db215a34824e82be5fe293d19a15e73a55de0
Godwin Adah <adahgodwin224@gmail.com>
2023-10-19 03:27:13 +0100

    nav

--- src/app/screens/projectscreen/projectscreen.component.ts
+++ src/app/screens/projectscreen/projectscreen.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./projectscreen.component.scss']
 })
 export class ProjectscreenComponent {
-
+  range: number[] = [1, 2, 3, 4, 5];
+  selectedIndex: number = 0;
+  setIndex(index: number) {
+    this.selectedIndex = index;
+  }
 }
