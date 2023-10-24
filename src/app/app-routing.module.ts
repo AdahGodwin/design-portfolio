5b90baf53cbd663cf35320e7ac96f065735b51e0
Godwin Adah <adahgodwin224@gmail.com>
2023-10-24 19:04:57 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomescreenComponent } from './screens/homescreen/homescreen.component';
+import { ProjectscreenComponent } from './screens/projectscreen/projectscreen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'home', pathMatch: "full"},
+  {path:'home', component: HomescreenComponent,},
+  {path:'projects', component: ProjectscreenComponent,},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
